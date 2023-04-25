bfd5979c4e819023a052d8cdd7406fbdce025e3d
KanazawaKeima730 <keima.kanazawa@bitbyte.co.jp>
2023-04-25 13:18:32 +0900

    Gemfile.lock fix

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module LineChatgptRailsapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
